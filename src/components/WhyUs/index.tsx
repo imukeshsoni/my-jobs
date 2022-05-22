6dc2145ab1a0cf7e0538bb7d5519b2ee8cd60e61
Mukesh Soni <msony552@gmail.com>
2022-05-22 22:55:43 +0530

    updated whyUS

--- src/components/WhyUs/index.tsx
+++ src/components/WhyUs/index.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, Typography } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
 
-import './styles.css'
+import './styles.css';
 
 interface CustomCard {
   title?: string;
@@ -31,31 +31,29 @@ function WhyUs(): JSX.Element {
       <div className="whyus__title"> Why Us</div>
 
       <div className="card__container">
-      {cards.map((card, index) => {
-        return (
-
-          <Card sx={{ maxWidth: 250 }} key={index}>
-            <CardContent>
-              <Typography
-                variant="h5"
-                component="div"
-                sx={{
-                  color: '#43AFFF',
-                  fontSize: 22,
-                  maxWidth: 140,
-                  fontWeight: '500',
-                    paddingBottom: 1
-                }}
-              >
-                {card.title}
-              </Typography>
-              <Typography variant="body2">{card.content}</Typography>
-            </CardContent>
-          </Card>
-
-);
-})}
-</div>
+        {cards.map((card, index) => {
+          return (
+            <Card sx={{ maxWidth: 290 }} key={index}>
+              <CardContent>
+                <Typography
+                  variant="h5"
+                  component="div"
+                  sx={{
+                    color: '#43AFFF',
+                    fontSize: 22,
+                    maxWidth: 140,
+                    fontWeight: '500',
+                    paddingBottom: 1,
+                  }}
+                >
+                  {card.title}
+                </Typography>
+                <Typography variant="body2">{card.content}</Typography>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </div>
     </>
   );
 }
