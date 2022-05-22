38612769edbb80add79e5a0e6482daa278dbf44f
Mukesh Soni <msony552@gmail.com>
2022-05-22 15:17:58 +0530

    added routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,17 +1,21 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/homePage";
-import LoginPage from "./pages/LoginPage";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ForgotPassword from './components/ForgotPassword';
+import SignUp from './components/SignUp';
+import HomePage from './pages/homePage';
+import LoginPage from './components/Login';
 
 const BaseRouter = () => {
-    return (
-        <div>
-            <Routes>
-                <Route  path="/" element={<HomePage/>} />
-                <Route  path="/login" element={<LoginPage/>} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default BaseRouter;
