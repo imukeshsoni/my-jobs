bf0978d1cbddf7a0fb7139834cf2f6204d7e86e9
Mukesh Soni <msony552@gmail.com>
2022-05-23 14:28:52 +0530

    updated header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,11 @@
-import { Button } from '@mui/material';
-import React, { FC } from 'react';
+import { Avatar, Button, Menu, MenuItem } from '@mui/material';
+import React, { FC, useEffect, useState } from 'react';
 import './styles.css';
 
 import logo from '../../assets/logo.svg';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthService } from '../../services/AuthService';
+import { StorageService } from '../../services/StorageService';
 
 interface Props {
   showLogin?: boolean;
@@ -14,7 +16,31 @@ const Header: FC<Props> = ({
   showLogin = true,
   buttonText = 'Login/Signup',
 }) => {
-  const handleLogin = () => {};
+  const authService = new AuthService(new StorageService());
+  let navigate = useNavigate();
+  const [userName, setUserName] = useState<string>('');
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const openMenu = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const closeMenu = () => {
+    setAnchorEl(null);
+  };
+
+  useEffect(() => {
+    const user = authService.getUserData();
+    if (user && user.name) {
+      setUserName(user.name);
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    authService.logoutUser();
+    navigate('/');
+  };
 
   const btn: JSX.Element = (
     <Link to="/login">
@@ -29,7 +55,26 @@ const Header: FC<Props> = ({
           <Link to="/">
             <img src={logo} alt="" />
           </Link>
-          {showLogin ? btn : ''}
+          <span>
+            {isLoggedIn ? (
+              <Avatar onClick={(e) => handleClick(e)}>{userName[0]}</Avatar>
+            ) : showLogin ? (
+              btn
+            ) : (
+              ''
+            )}
+          </span>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={openMenu}
+            onClose={closeMenu}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+          >
+            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+          </Menu>
         </div>
       </div>
     </>
